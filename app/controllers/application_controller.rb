8018ab894a16d17531a80976b2b32ed51f05df8b
Aaron Barker <voxxsound@gmail.com>
2010-07-30 17:29:31 -0400

    internationalization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@
 class ApplicationController < ActionController::Base
   layout "store"
   before_filter :authorize, :except => :login
+  before_filter :set_locale
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
@@ -11,6 +12,26 @@ class ApplicationController < ActionController::Base
   # filter_parameter_logging :password
   
 protected
+
+  def set_locale
+    session[:locale] = params[:locale] if params[:locale]
+    I18n.locale = session[:locale] || I18n.default_loacle
+    
+    locale_path = "#{LOCALES_DIRECTORY}#{I18n.locale}.yml"
+    
+    unless I18n.load_path.include? locale_path
+      I18n.load_path << locale_path
+      I18n.backend.send(:init_translations)
+    end
+    
+  rescue Exception => err
+    logger.error err
+    flash.now[:notice] = "#{I18n.locale} translation not available."
+    
+    I18n.load_path -= [locale_path]
+    I18n.locale = session[:locale] = I18n.default_locale
+  end
+  
   def authorize
     unless User.find_by_id(session[:user_id])
       flash[:notice] = "Please log in"
